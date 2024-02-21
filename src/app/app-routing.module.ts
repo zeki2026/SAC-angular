53ed624cd9400461bf2d6a1ad52da66776960145
IVAN EZEQUIEL CABALLERO CRUZ <ACAZW@SertecDF.mx>
2024-02-21 11:53:22 -0600

    layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { sessionGuard } from './core/guards/session.guard';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
+  },
+  {
+    path:'home',
+    component:HomePageComponent,
+    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule),
+    canActivate:[sessionGuard]
+  },
+  {
+    path:'**',
+    redirectTo:'/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    DataTablesModule
   ],
-  providers: [],
+  providers: [
+    CookieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
